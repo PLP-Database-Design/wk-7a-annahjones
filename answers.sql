4c42870d7ef6314f098602b1e8306552227eaff4
Ann Waitherero <annmigwi70@gmail.com>
2025-04-21 18:05:06 +0300

    Update answers.sql

--- answers.sql
+++ answers.sql
@@ -30,6 +30,20 @@ VALUES
 (102, 'Jane Smith'),
 (103, 'Emily Clark');
 
+--OUTPUT--
+OrderID | CustomerName | Product
+101 | John Doe | Laptop
+101 | John Doe | Mouse
+102 | Jane Smith | Tablet
+102 | Jane Smith | Keyboard
+102 | Jane Smith | Mouse
+103 | Emily Clark | Phone
+    
+OrderID | CustomerName
+101 | John Doe
+102 | Jane Smith
+103 | Emily Clark
+
 -- Question 2: Achieving 2NF (Second Normal Form)
 -- Create OrderItems table (OrderID + Product -> Quantity)
 CREATE TABLE OrderItems (
@@ -48,3 +62,12 @@ VALUES
 (102, 'Keyboard', 1),
 (102, 'Mouse', 2),
 (103, 'Phone', 1);
+
+--OUTPUT--
+OrderID | Product | Quantity
+101 | Laptop | 2
+101 | Mouse | 1
+102 | Tablet | 3
+102 | Keyboard | 1
+102 | Mouse | 2
+103 | Phone | 1
